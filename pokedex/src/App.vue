d8ce2c10c5997f1ee5dc7c633a2585dbd27fc7eb
Thawan Marques <thawan28032001@gmail.com>
2023-02-26 05:06:45 -0300

    Começo do Projeto

---
+++ pokedex/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    hello word
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      pokemons: []
+    }
+  },
+  created: function() {
+    axios.get('https://pokeapi.co/api/v2/pokemon?limit=151&offset=0.').then(res => {
+      this.pokemon = res.data.results
+      console.log(this.pokemon)
+    })
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
