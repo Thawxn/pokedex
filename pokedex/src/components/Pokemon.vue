3595e8f206aaf2e3e424e74a8db223c39908a3f6
Thawan Marques <thawan28032001@gmail.com>
2023-02-28 00:15:29 -0300

    Melhorando Componentes

--- pokedex/src/components/Pokemon.vue
+++ pokedex/src/components/Pokemon.vue
@@ -3,6 +3,26 @@
     <div>
         <h1>{{num}} {{name | upper}}</h1>
         <small>{{url}}</small>
+
+        <div class="card">
+            <div class="card-image">
+                <figure class="image is-4by3">
+                <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+                </figure>
+            </div>
+            <div class="card-content">
+                <div class="media">
+                    <div class="media-content">
+                        <p class="title is-4">John Smith</p>
+                        <p class="subtitle is-6">@johnsmith</p>
+                    </div>
+                </div>
+
+                <div class="content">
+
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -15,6 +35,7 @@ export default {
             this.pokemon.type = res.data.types[0].type.name;
             this.pokemon.front = res.data.sprites.front_default;
             this.pokemon.back = res.data.sprites.back_default;
+
             console.log(this.pokemon);
         })
     },
